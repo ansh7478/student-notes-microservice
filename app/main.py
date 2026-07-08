e2ec99efd3b19ca5872200d98f3f1d48b0b12ae1
ansh7478 <ansingh@stud.macromedia.de>
2026-07-08 15:31:24 +0200

    Complete CRUD operations for Student Notes API

--- app/main.py
+++ app/main.py
@@ -1,4 +1,12 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, HTTPException
+from sqlalchemy.orm import Session
+from typing import List
+
+from app.database import Base, engine, get_db
+from app import schemas, crud, models
+
+# Create database tables automatically
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI(
     title="Student Notes Microservice",
@@ -9,6 +17,44 @@ app = FastAPI(
 
 @app.get("/")
 def home():
-    return {
-        "message": "Welcome to Student Notes Microservice!"
-    }
+    return {"message": "Welcome to Student Notes Microservice!"}
+
+
+@app.post("/notes", response_model=schemas.NoteResponse)
+def create_note(note: schemas.NoteCreate, db: Session = Depends(get_db)):
+    return crud.create_note(db, note)
+
+
+@app.get("/notes", response_model=List[schemas.NoteResponse])
+def get_notes(db: Session = Depends(get_db)):
+    return crud.get_notes(db)
+
+
+@app.get("/notes/{note_id}", response_model=schemas.NoteResponse)
+def get_note(note_id: int, db: Session = Depends(get_db)):
+    note = crud.get_note(db, note_id)
+
+    if not note:
+        raise HTTPException(status_code=404, detail="Note not found")
+
+    return note
+
+
+@app.put("/notes/{note_id}", response_model=schemas.NoteResponse)
+def update_note(note_id: int, note: schemas.NoteUpdate, db: Session = Depends(get_db)):
+    updated_note = crud.update_note(db, note_id, note)
+
+    if not updated_note:
+        raise HTTPException(status_code=404, detail="Note not found")
+
+    return updated_note
+
+
+@app.delete("/notes/{note_id}")
+def delete_note(note_id: int, db: Session = Depends(get_db)):
+    deleted_note = crud.delete_note(db, note_id)
+
+    if not deleted_note:
+        raise HTTPException(status_code=404, detail="Note not found")
+
+    return {"message": "Note deleted successfully"}
